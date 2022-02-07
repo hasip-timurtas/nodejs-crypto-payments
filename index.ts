092f30f642473845eef88b0fde0d9a4e9deeade6
Hasip Timurtaş <hasip.timurtas@gmail.com>
2022-02-07 21:29:54 +0000

    Docker and yalm config added.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 import Transactions from './app/transactions'
 
 async function run(): Promise<void> {
-  await connect('mongodb+srv://mht:15yb88dycf@cluster0.gtwkz.mongodb.net/finans?retryWrites=true&w=majority');
+  await connect('mongodb://mongo:27017/kraken');
   const transactions = new Transactions();
   const data = await transactions.getDepositInformation();
 }
